cafcb30950fba524273c2dd545375e4e59840cf3
Calvin So <Calvin So>
2016-02-18 14:33:23 -0500

    Initial upload round two.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject follow;
+    Vector3 difference;
+	
+    void Start()
+    {
+        difference = follow.transform.position - transform.position;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        transform.position = follow.transform.position - difference;
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# RubeGoldberg
+Goldberg Unity Assignment
