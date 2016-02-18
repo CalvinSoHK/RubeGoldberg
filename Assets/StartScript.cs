9429f11c56d7e59bede77410575567eb8375c007
Calvin So <Calvin So>
2016-02-18 15:22:19 -0500

    PreliminaryWork

---
+++ Assets/StartScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            //GetComponent<Rigidbody>().AddForce(new Vector3(0, 500f, 0));
+        }
+	}
+}
